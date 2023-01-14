b50c86e26dd89826e8a6e2b09e8a8ff6284d42e7
AndrePort <aporto-@hotmail.com>
2023-01-14 14:48:07 -0300

    feat: finish create, update and delete items

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./index.css";
 
-export const ListCard = ({ item }) => {
+export const ListCard = ({ item, onClick }) => {
   const [checked, setChecked] = useState(false);
   return (
-    <div className="list-card-container">
+    <div className="list-card-container" onClick={() => onClick(item)}>
       <div className="checkbox">
         <input
           onChange={() => setChecked(!checked)}
@@ -16,9 +16,9 @@ export const ListCard = ({ item }) => {
       </div>
       <div className="list-card-text-container">
         <span className="list-card-title">{item?.name}</span>
-        <span className="list-card-subtitle">{item?.quatity} unidades</span>
+        <span className="list-card-subtitle">{item?.quantity} unidades</span>
       </div>
-      <img className="arrow-icon" src="arrow-icon" alt="arrow-icon" />
+      <img className="arrow-icon" src="../../.././img/arrow.svg" alt="arrow-icon" />
     </div>
   );
 };
